abf81b3f2b5aed6d6d05dfad9c4546e470ee835c
Nicklas Holmberg <45942181+nishog04@users.noreply.github.com>
2020-01-12 10:44:10 +0100

    Prettywrite

--- src/main/java/com/example/labb1/Controller.java
+++ src/main/java/com/example/labb1/Controller.java
@@ -1,44 +1,39 @@
 package com.example.labb1;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import java.util.Arrays;
+import java.io.IOException;
 
 @RestController
 public class Controller {
 
     private final String apiKey = "?api_key=3000256a09175cd76b573f34c8245b50";
-    private String URL = "https://api.themoviedb.org/3/movie/";
+    private final String url = "https://api.themoviedb.org/3/movie/";
 
     @Autowired
     RestTemplate restTemplate;
 
     @RequestMapping(value = "/movies")
-    public String getTopRatedMovies() {
+    public String getTopRatedMovies() throws IOException {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        return restTemplate.exchange(
-                URL + "top_rated" + apiKey, HttpMethod.GET, entity, String.class).getBody();
-
+        String jsonInput = restTemplate.getForObject(url + "top_rated" + apiKey, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        Object json = mapper.readValue(jsonInput, Object.class);
+        String jsonOutput = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        return jsonOutput;
     }
 
     @RequestMapping(value = "/favorites")
-    public String getFavoriteMovie() {
+    public String getFavoriteMovie() throws JsonProcessingException {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        return restTemplate.exchange(
-                URL + "278" + apiKey, HttpMethod.GET, entity, String.class).getBody();
+        String jsonInput = restTemplate.getForObject(url + "278" + apiKey, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        Object json = mapper.readValue(jsonInput, Object.class);
+        String jsonOutput = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        return jsonOutput;
     }
 }
